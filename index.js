e6dd8d1b1b96e75ca44a762afeb8cd74c1b8f4ee
shwetank08 <shwetanksingh.in@gmail.com>
2022-10-17 23:16:03 +0530

    second commit

--- index.js
+++ index.js
@@ -4,51 +4,83 @@ const next = document.querySelector(".next");
 const backward = document.querySelector(".backward");
 const forward = document.querySelector(".forward");
 const audio = document.querySelector("audio");
-const image = document.querySelector('img');
-const dummy = document.querySelector('#dummy');
-const progressbar = document.querySelector('.progressbar');
+const image = document.querySelector("img");
+const dummy = document.querySelector("#dummy");
+const progressbar = document.querySelector(".progressbar");
+const Mname = document.querySelector(".name");
+const volumeBtn = document.querySelector(".vol");
+const p = document.querySelector('progress');
+const start = document.querySelector('#start');
+const end = document.querySelector('#end');
+const input = document.querySelector('input');
+
 
 
 const musicLib = [
-    {
-        img: "./Images/Interstellar.jpg",
-        music:  "./Audio File/Interstellar.mp3"
-    },
-    {
-        img: "./Images/ludovinco.jpg",
-        music:  "./Audio File/Ludovico Einaudi Experience - Bgm.mp3"
-    },
-    {
-        img: "./Images/Your Lie In April.png",
-        music:  "./Audio File/Shigatsu Wa Kimi No Uso.mp3"
-    },
-    {
-        img: "./Images/Your Name.png",
-        music: "./Audio File/Kimi No Na Wa Sparkle ! Kimi No Na Wa.mp3"
-    }
-]
-
+  {
+    img: "./Images/Interstellar.jpg",
+    music: "./Audio File/Interstellar.mp3",
+    name: "Interstellar",
+  },
+  {
+    img: "./Images/ludovinco.jpg",
+    music: "./Audio File/Ludovico Einaudi Experience - Bgm.mp3",
+    name: "Experience",
+  },
+  {
+    img: "./Images/Your Lie In April.png",
+    music: "./Audio File/Shigatsu Wa Kimi No Uso.mp3",
+    name: "Your Lie In April OST",
+  },
+  {
+    img: "./Images/Your Name.png",
+    music: "./Audio File/Kimi No Na Wa Sparkle ! Kimi No Na Wa.mp3",
+    name: "Sparkle",
+  },
+];
 
 let i = 0;
 
 function progress(){
-  let p = document.getElementsByClassName('myprogressBar')
-  progress_percentage = Math.floor((audio.currentTime)/(audio.duration))*100;
-  p.style.width = progress_percentage + .1;
+  let curTime = audio.currentTime;
+  let duration = audio.duration;
+  progress_percentage = (curTime)*100/(duration);
+  p.value = Math.floor(progress_percentage);
+
+
+  let startTimeSec = Math.floor(curTime);
+  if(startTimeSec<10){
+    startTimeSec='0'+startTimeSec;
+  }
+  let startTimeMin = 0;
+
+  let endTimeMin = 0;
+  let endTimeSec = Math.floor(audio.duration);
+  if(endTimeSec<10){
+    endTimeSec='0'+endTimeSec;
+  }
+
+  let Etime = `${endTimeMin}:${endTimeSec}`
+  let Stime = `${startTimeMin}:${startTimeSec}`
+
+  start.innerHTML = Stime;
+  end.innerHTML = Etime;
+
 }
 
-
 const playMusic = () => {
-  console.log(audio.paused);
+  const song = musicLib[i].name;
+  Mname.innerHTML = song;
   if (!audio.paused) {
-    image.classList.remove('.rotate')
+    image.classList.remove(".rotate");
     audio.pause();
-    play.innerHTML = `<i class="fa-solid fa-play"></i>`
-    image.classList.remove('rotate');
+    play.innerHTML = `<i class="fa-solid fa-play"></i>`;
+    image.classList.remove("rotate");
+  
   } else {
     audio.play();
-    play.innerHTML = `<i class="fa-solid fa-pause"></i>`
-    image.classList.add('rotate');
+    play.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+    image.classList.add("rotate");
   }
 };
 
@@ -56,54 +88,86 @@ const backwardMusic = () => {
   if (audio.currentTime - 5 >= 0) {
     audio.currentTime = audio.currentTime - 5;
     audio.play();
-  }else{
+    play.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+    image.classList.add("rotate");
+  } else {
     audio.currentTime = 0;
     audio.play();
+    play.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+    image.classList.add("rotate");
   }
 };
 const forwardMusic = () => {
   if (audio.currentTime + 5 > audio.duration) {
     audio.currentTime = audio.duration;
     audio.play();
-  }else{
+    play.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+    image.classList.add("rotate");
+  } else {
     audio.currentTime = audio.currentTime + 5;
     audio.play();
+    play.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+    image.classList.add("rotate");
   }
 };
 const nextTrack = () => {
-  console.log("nextTrack");
-    let src = musicLib[i].music;
-    let img = musicLib[i].img;
+  i += 1;
+  i = i % 4;
+  const song = musicLib[i].name;
+  Mname.innerHTML = song;
+  let src = musicLib[i].music;
+  let img = musicLib[i].img;
 
-    image.src=img;
-    audio.src=src;
+  image.src = img;
+  audio.src = src;
 
-    audio.play();
-    setInterval(progress,100);
-    play.innerHTML = `<i class="fa-solid fa-pause"></i>`
+  audio.play();
+  image.classList.add("rotate");
 
-    i+=1;
-    i = i%4;
-}
+  // setInterval(progress,100);
+  play.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+};
 const prevTrack = () => {
-  console.log("prevTrack");
-    let src = musicLib[i].music; 
-    let img = musicLib[i].img;
-    console.log(img);
-    image.setAttribute(
-        'src',
-        img
-    );
-    audio.src=src;
+  i -= 1;
+  if (i < 0) {
+    i += 4;
+  }
+  i %= 4;
 
-    audio.play();
-    setInterval(progress,100);
-    play.innerHTML = `<i class="fa-solid fa-pause"></i>`
+  const song = musicLib[i].name;
+  Mname.innerHTML = song;
+  let src = musicLib[i].music;
+  let img = musicLib[i].img;
+  console.log(img);
+  image.setAttribute("src", img);
+  audio.src = src;
 
-    i-=1;
-    i%=4;
+  audio.play();
+  image.classList.add("rotate");
+
+  // setInterval(progress,100);
+  play.innerHTML = `<i class="fa-solid fa-pause"></i>`;
+};
+
+input.classList.add('hidden');
+
+function controlVolume(){
+  input.classList.remove('hidden');
+  let vol = input.value;
+  vol/=100;
+  console.log(vol);
+  audio.volume = vol;
 }
 
+function muteVolume(){
+  
+}
+
+volumeBtn.addEventListener('click',muteVolume);
+volumeBtn.addEventListener('mouseover',controlVolume);
+volumeBtn.addEventListener('mouseout',()=>{
+  input.classList.add('hidden')
+});
 forward.addEventListener("click", forwardMusic);
 backward.addEventListener("click", backwardMusic);
 play.addEventListener("click", playMusic);
@@ -111,12 +175,13 @@ next.addEventListener("click", nextTrack);
 prev.addEventListener("click", prevTrack);
 
 const check = () => {
-  if(audio.currentTime === audio.duration){
-    console.log('track ended');
-    i+=1;
+  if (audio.currentTime === audio.duration) {
+    console.log("track ended");
+    i += 1;
     nextTrack();
   }
-}
+};
 
-const time = setInterval(check,1000);
+const time = setInterval(check, 1000);
+setInterval(progress,950);
 
